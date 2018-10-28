857a9767a92d32a0882e82d6b0da943b13d0b82c
vlad3k <vlad3k@bk.ru>
2018-10-28 19:47:36 +0300

    edited code for menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,25 +1,20 @@
-var menu = document.getElementById("menu");
-var hamb = document.getElementById("hamb");
-menu.classList.remove("main-nav--no-js");
-menu.classList.add("main-nav--close");
-hamb.classList.remove("page-header__toggle--no-js");
+(function () {
+  var menu = document.getElementById("menu");
+  var hamb = document.getElementById("hamb");
+  menu.classList.remove("main-nav--no-js");
+  menu.classList.add("main-nav--close");
+  hamb.classList.remove("page-header__toggle--no-js");
+  hamb.classList.add("page-header__toggle--close");
 
-hamb.addEventListener("click", function() {
-  if (menu.classList.contains("main-nav--open")) {
-    menu.classList.remove("main-nav--open");
-    menu.classList.add("main-nav--close");
-    hamb.classList.remove("page-header__toggle--open")
-    hamb.classList.add("page-header__toggle--close")
-  } else {
-    menu.classList.remove("main-nav--close");
-    menu.classList.add("main-nav--open");
-    hamb.classList.remove("page-header__toggle--close")
-    hamb.classList.add("page-header__toggle--open")
-  }
-});
+  hamb.addEventListener("click", function() {
+    menu.classList.toggle("main-nav--close");
+    hamb.classList.toggle("page-header__toggle--open");
+  });
+})();
 
-ymaps.ready(function() {
-  var myMap = new ymaps.Map('map', {
+(function () {
+  ymaps.ready(function() {
+    var myMap = new ymaps.Map('map', {
       center: [59.938934, 30.319455],
       zoom: 14,
       controls: []
@@ -34,6 +29,7 @@ ymaps.ready(function() {
       iconImageOffset: [-54, -106],
     });
 
-  myMap.geoObjects
-    .add(myPlacemark);
-});
+    myMap.geoObjects
+      .add(myPlacemark);
+  });
+})();
